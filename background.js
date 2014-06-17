8594649292e09886bd0128cd055eb4ca1e5f93d9
tatianatylosky@gmail.com <tatianajtylosky@Tatianas-MacBook-Air.local>
2014-06-17 18:22:07 -0400

    initial
    Makes your background a flower, chrome extension!!!

---
+++ background.js
@@ -0,0 +1,13 @@
+document.body.style.backgroundImage="url('http://24.media.tumblr.com/7ec15487856dbf285d695e5a4f8e12ef/tumblr_n6rzwbv3ji1st5lhmo1_1280.jpg')";
+// document.body.style.backgroundImage="('flower.jpg')";
+document.body.style.backgroundRepeat="no-repeat";
+document.body.style.backgroundPosition="center";
+document.body.style.backgroundAttachment="fixed";
+document.body.style.backgroundSize="100% auto";
+
+
+// object.style.backgroundSize="60px 80px";
+
+// background-attachment:fixed;
+
+// background-position:center; 
